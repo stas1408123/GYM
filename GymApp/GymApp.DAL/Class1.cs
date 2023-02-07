c1be3b878837655288133a75221e110b9fc208bd
hinkevich <hinkevich@gmail.com>
2023-02-07 15:02:22 +0300

    Add some entiries

--- GymApp/GymApp.DAL/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GymApp.DAL
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ GymApp/GymApp.DAL/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.DAL.Entities
+{
+     public class Order
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public double Coust { get; set; }
+
+        //navigation property
+        public int PeopleId { get; set; }
+
+        public People People { get; set; }
+
+       
+    }
+}
