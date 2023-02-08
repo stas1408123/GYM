3bfc186b605a64f3a90c2bfa57c267b02b2545d7
Stanislau <stas1408123@gmail.com>
2023-02-08 16:37:26 +0300

    upd

--- GymApp/GymAppApi/Program.cs
+++ GymApp/GymAppApi/Program.cs
@@ -2,11 +2,11 @@ using GYM.API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<GymDbContext>(opt => opt.UseSqlServer("name=ConnectionStrings:GymDbDefault"));
+builder.Services.AddDbContext<GymDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
