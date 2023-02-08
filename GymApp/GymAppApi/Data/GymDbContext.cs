85564d5dde50b70ecdd7258fba713e40ce5e8de7
hinkevich <hinkevich@gmail.com>
2023-02-08 11:46:47 +0300

    add changes

--- GymApp/GymAppApi/Data/GymDbContext.cs
+++ GymApp/GymAppApi/Data/GymDbContext.cs
@@ -8,11 +8,10 @@ namespace GYM.API.Data
     {
         public GymDbContext(DbContextOptions<GymDbContext> options):base(options)
         {
-                
         }
+
         public DbSet<Couch> Couches { get; set; } = null!;
         public DbSet<Order> Orders { get; set; } = null!;
         public DbSet<Visitor> Visitors { get; set; } = null!;
-
     }
 }
