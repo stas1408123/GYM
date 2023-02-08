fb9ffa250c3cce1bc120ba8ff5f51fb0e80e8ec3
hinkevich <51876492+hinkevich@users.noreply.github.com>
2023-02-08 11:11:08 +0300

    Feature/add madels to api (#3)
    * Add solution and web Api
    
    * Add models
    
    * correct some mistakes in models
    
    * Add attributes to models
    
    * Rename project

---
+++ GymApp/GymAppApi/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymAppApi.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 1)]
+        public string Title { get; set; } = null!;
+        [Required]
+        [StringLength(150, MinimumLength = 3)]
+        public string Description { get; set; } = null!;
+        [Required]
+        public double Cost { get; set; }
+        public DateTime Date { get; set; }
+
+        //Navigation property
+        public int VisitorId { get; set; }
+        public Visitor Visitor { get; set; } = null!;
+
+    }
+}
