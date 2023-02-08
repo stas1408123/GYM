5895429d50c1b5b39e6f6df662a45d92aea6aa0c
hinkevich <hinkevich@gmail.com>
2023-02-08 11:34:04 +0300

    Add Db Context

---
+++ GymApp/GymAppApi/Data/GymDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GymAppApi.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace GYM.API.Data
+{
+    public class GymDbContext:DbContext
+    {
+       
+        public DbSet<Couch> Couches { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Visitor> Visitors { get; set; } = null!;
+
+    }
+}
