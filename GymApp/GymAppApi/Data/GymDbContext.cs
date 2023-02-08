5e87b2bdddc9dc62d4862e8495aa4953f3137b3d
hinkevich <hinkevich@gmail.com>
2023-02-08 11:37:37 +0300

    add ctor

--- GymApp/GymAppApi/Data/GymDbContext.cs
+++ GymApp/GymAppApi/Data/GymDbContext.cs
@@ -6,7 +6,10 @@ namespace GYM.API.Data
 {
     public class GymDbContext:DbContext
     {
-       
+        public GymDbContext(DbContextOptions<GymDbContext> options):base(options)
+        {
+                
+        }
         public DbSet<Couch> Couches { get; set; } = null!;
         public DbSet<Order> Orders { get; set; } = null!;
         public DbSet<Visitor> Visitors { get; set; } = null!;
