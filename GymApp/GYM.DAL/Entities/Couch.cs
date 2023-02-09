92038708eb70f9fa915c18d0d03500dc1c566cb2
hinkevich <hinkevich@gmail.com>
2023-02-09 11:55:40 +0300

    Add entities to DAL

---
+++ GymApp/GYM.DAL/Entities/Couch.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GYM.API.Models
+{
+    public class Couch
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 1)]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        [StringLength(150, MinimumLength = 1)]
+        public string LastName { get; set; } = null!;
+        [Required]
+        [StringLength(750, MinimumLength = 3)]
+        public string Describe { get; set; } = null!;
+
+        //Navigation property
+        public int VisitorId { get; set; }
+        public List<Visitor> Visitors { get; set; } = null!;
+    }
+}
---
+++ GymApp/GYM.DAL/Entities/Visitor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GYM.API.Models
+{
+    public class Visitor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 1)]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        [StringLength(150, MinimumLength = 1)]
+        public string LastName { get; set; } = null!;
+
+        //Navigation properties
+        public List<Couch> Couches { get; set; } = null!;
+        public List<Order> Orders { get; set; } = null!;
+    }
+}
