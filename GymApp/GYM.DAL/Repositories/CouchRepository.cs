08de09da895e8af8c15cfd9d4a64067df5e3140a
hinkevich <hinkevich@gmail.com>
2023-02-13 16:02:38 +0300

    Add generic repository

--- GymApp/GYM.DAL/Repositories/CouchRepository.cs
+++ GymApp/GYM.DAL/Repositories/CouchRepository.cs
@@ -1,55 +1,12 @@
 ﻿using GYM.DAL.EF;
 using GYM.DAL.Entities;
-using GYM.DAL.Repositories.Abstractions;
-using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
 
 namespace GYM.DAL.Repositories
 {
-    public class CouchRepository : IRepository<CouchEntity>
+    public class CouchRepository : GenericRepository<CouchEntity>
     {
-        private readonly GymAppDbContext _context;
-        public CouchRepository(GymAppDbContext context)
+        public CouchRepository(GymAppDbContext context) : base(context)
         {
-            _context = context;
-        }
-
-        public async Task Create(CouchEntity item)
-        {
-            _context.CouchEntities.Add(item);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<bool> Delete(int id)
-        {
-            var item = await _context.CouchEntities.FindAsync(id);
-            if (item != null)
-            {
-                _context.CouchEntities.Remove(item);
-                return true;
-            }
-            return false;
-        }
-
-        public async Task<IEnumerable<CouchEntity>> Get(Expression<Func<CouchEntity, bool>> predicate)
-        {
-            return await _context.CouchEntities.Where(predicate).ToListAsync();
-        }
-
-        public async Task<CouchEntity?> Get(int id)
-        {
-            return await _context.CouchEntities.FindAsync(id);
-        }
-
-        public async Task<IEnumerable<CouchEntity>> GetAll()
-        {
-            return await _context.CouchEntities.AsNoTracking().ToListAsync();
-        }
-
-        public async Task Update(CouchEntity item)
-        {
-            _context.CouchEntities.Update(item);
-            await _context.SaveChangesAsync();
         }
     }
 }
---
+++ GymApp/GYM.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using GYM.DAL.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace GYM.DAL.Repositories
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity?> Get(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public async Task Create(TEntity item)
+        {
+            _dbSet.Add(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(TEntity item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
