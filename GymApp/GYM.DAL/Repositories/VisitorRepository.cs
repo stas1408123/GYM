81c7bea8d143cc2435e2a5c240df9bd0d4a5899a
hinkevich <51876492+hinkevich@users.noreply.github.com>
2023-02-13 17:04:59 +0300

    Add generic repository (#13)

--- GymApp/GYM.DAL/Repositories/VisitorRepository.cs
+++ GymApp/GYM.DAL/Repositories/VisitorRepository.cs
@@ -1,53 +1,12 @@
 ﻿using GYM.DAL.EF;
 using GYM.DAL.Entities;
-using GYM.DAL.Repositories.Abstractions;
-using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
 
 namespace GYM.DAL.Repositories
 {
-    public class VisitorRepository : IRepository<VisitorEntity>
+    public class VisitorRepository : GenericRepository<VisitorEntity>
     {
-        private readonly GymAppDbContext _context;
-        public VisitorRepository(GymAppDbContext context)
+        public VisitorRepository(GymAppDbContext context) : base(context)
         {
-            _context = context;
-        }
-        public async Task Create(VisitorEntity item)
-        {
-            _context.VisitorEntities.Add(item);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<bool> Delete(int id)
-        {
-            var item = await _context.VisitorEntities.FindAsync(id);
-            if (item != null)
-            {
-                _context.VisitorEntities.Remove(item);
-                return true;
-            }
-            return false;
-        }
-
-        public async Task<IEnumerable<VisitorEntity>> Get(Expression<Func<VisitorEntity, bool>> predicate)
-        {
-            return await _context.VisitorEntities.Where(predicate).ToListAsync();
-        }
-        public async Task<VisitorEntity?> Get(int id)
-        {
-            return await _context.VisitorEntities.FindAsync(id);
-        }
-
-        public async Task<IEnumerable<VisitorEntity>> GetAll()
-        {
-            return await _context.VisitorEntities.AsNoTracking().ToListAsync();
-        }
-
-        public async Task Update(VisitorEntity item)
-        {
-            _context.VisitorEntities.Update(item);
-            await _context.SaveChangesAsync();
         }
     }
 }
