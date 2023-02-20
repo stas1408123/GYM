328e0f7cdcc44538dc22aff648074bf24efc8634
hinkevich <hinkevich@gmail.com>
2023-02-20 17:56:03 +0300

    Add cors

---
+++ GymApp/GymAppApi/Extensions/CustomCors.cs
@@ -0,0 +1,16 @@
+﻿namespace GYM.API.Extensions
+{
+    public static class CustomCors
+    {
+        private static readonly string DefaultName = "DefaultPolicy";
+
+        public static void AddCustomCors(this IServiceCollection services)
+        {
+            services.AddCors(opt => opt.AddPolicy(DefaultName, builder => builder
+                .WithOrigins("http://localhost:46409", "https://localhost:7163", "http://localhost:5227")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+            ));
+        }
+    }
+}
