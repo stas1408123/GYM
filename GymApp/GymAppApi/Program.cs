764e61a3038638985827315735b9f6401895cf29
hinkevich <hinkevich@gmail.com>
2023-02-21 09:32:16 +0300

    change mistake in program

--- GymApp/GymAppApi/Program.cs
+++ GymApp/GymAppApi/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(CustomCors.DEFAULT_CORS_POLICY);
+app.UseCors(CustomCors.DefaultCorsPolicy);
 
 app.UseMiddleware<CustomExceptionHandler>();
 
