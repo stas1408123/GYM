758b738871e0ecd78e30c355ab91c579e38e2c5c
hinkevich <51876492+hinkevich@users.noreply.github.com>
2023-02-21 09:47:50 +0300

    Feature/nmd 292 add cors (#19)
    * Add cors
    
    * Change program
    
    * Add const to UseCors in program
    
    * change mistake in program

---
+++ GymApp/GymAppApi/Extensions/CustomCors.cs
@@ -0,0 +1,16 @@
+﻿namespace GYM.API.Extensions
+{
+    public static class CustomCors
+    {
+        public const string DefaultCorsPolicy = "DefaultPolicy";
+
+        public static void AddCustomCors(this IServiceCollection services)
+        {
+            services.AddCors(opt => opt.AddPolicy(DefaultCorsPolicy, builder => builder
+                .WithOrigins("http://localhost:46409", "https://localhost:7163", "http://localhost:5227")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+            ));
+        }
+    }
+}
