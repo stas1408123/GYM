d860fe42fb02a4dece21aefd4bee189a4e629538
hinkevich <51876492+hinkevich@users.noreply.github.com>
2023-03-02 09:43:55 +0300

    Feature/NMD-298 create and configure auth db context (#25)
    * Add Quickstart UI template
    
    * Add auth dbContext
    
    * Remove mistakes

---
+++ IdentityServer/IdentityServer/Data/AuthApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Data
+{
+    public class AuthApplicationContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public AuthApplicationContext(DbContextOptions<AuthApplicationContext> options)
+            : base(options)
+        {
+        }
+    }
+}
