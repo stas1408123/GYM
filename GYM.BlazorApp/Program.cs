ad914ae404111475acd9d1527b30a39e45fa32d5
hinkevich <hinkevich@gmail.com>
2023-03-20 16:02:03 +0300

    Add authentication for blazor client

--- GYM.BlazorApp/Program.cs
+++ GYM.BlazorApp/Program.cs
@@ -2,8 +2,13 @@ using GYM.BlazorApp.Data;
 using GYM.BlazorApp.Data.ViewModels;
 using GYM.BlazorApp.Interfaces;
 using GYM.BlazorApp.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -13,6 +18,43 @@ builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<IGenericService<CouchViewModel>, CouchesService>();
 builder.Services.AddScoped<IGenericService<OrderViewModel>, OrdersService>();
 builder.Services.AddScoped<IGenericService<VisitorViewModel>, VisitorsService>();
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme,
+        options =>
+        {
+            options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.SignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            options.Authority = "https://localhost:7181";
+            //options.Authority = "https://demo.identityserver.com/Account/Login";
+            options.ClientId = "mvc";
+            options.ClientSecret = "secret";
+
+            // When set to code, the middleware will use PKCE protection
+            options.ResponseType = "code";
+
+            // The "openid" and "profile" scopes are added automatically,
+            // so we don't need to add them for this test
+            //options.Scope.Add("openid");
+            //options.Scope.Add("profile");
+
+            // Save the tokens we receive from the IDP
+            options.SaveTokens = true;
+
+            // It's recommended to always get claims from the 
+            // UserInfoEndpoint during the flow. 
+            //options.GetClaimsFromUserInfoEndpoint = true;
+        });
+
+
+
 
 var app = builder.Build();
 
@@ -29,6 +71,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapBlazorHub();
 
