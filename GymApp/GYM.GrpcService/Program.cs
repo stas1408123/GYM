320a3a1961aada68ff3909a1ec186a14a376b314
hinkevich <hinkevich@gmail.com>
2023-03-23 15:12:37 +0300

    NMD-355 Add swagger to gRPC

--- GymApp/GYM.GrpcService/Program.cs
+++ GymApp/GYM.GrpcService/Program.cs
@@ -1,4 +1,5 @@
 using GYM.GrpcService.Services;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,9 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc().AddJsonTranscoding();
+
+builder.Services.AddGrpcSwagger().AddSwaggerGen(c => c.SwaggerDoc("v1",
+    new OpenApiInfo { Title = "gRPC trans-coding", Version = "v1" }));
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
