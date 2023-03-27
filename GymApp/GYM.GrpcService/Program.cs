33bf8ec4b57de7c022493c8d8dfa7cbf8046cdfb
hinkevich <51876492+hinkevich@users.noreply.github.com>
2023-03-27 17:41:14 +0300

    NMD-353 Add CRUD for Orders (#43)
    * NMD-353 Add CRUD for Orders
    
    * NMD-353 Add endpoint for swagger

--- GymApp/GYM.GrpcService/Program.cs
+++ GymApp/GYM.GrpcService/Program.cs
@@ -26,9 +26,9 @@ app.UseSwaggerUI(c =>
 });
 
 // Configure the HTTP request pipeline.
+app.MapGrpcService<OrdersApiService>();
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<CouchesApiService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
- 
