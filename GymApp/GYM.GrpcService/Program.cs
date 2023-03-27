1a0ede3d7d272c376c20283e37947f7d08d5f082
hinkevich <hinkevich@gmail.com>
2023-03-27 11:25:48 +0300

    NMD-352 add changes

--- GymApp/GYM.GrpcService/Program.cs
+++ GymApp/GYM.GrpcService/Program.cs
@@ -31,3 +31,4 @@ app.MapGrpcService<CouchesApiService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+ 
