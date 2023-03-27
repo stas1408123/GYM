3070ec8863d7ef73b63e49719efdd00a567cdf72
hinkevich <51876492+hinkevich@users.noreply.github.com>
2023-03-27 12:19:23 +0300

    Feature/nmd 352 add service for coaches(crud) (#42)
    * NMD-352 resolve conflicts
    
    * NMD-352 add service for coaches
    
    * NMD-352 add some changes
    
    * NMD-352 restore dependencies
    
    * NMD-352 add changes
    
    * NMD-352 Remove coments

--- GymApp/GYM.GrpcService/Program.cs
+++ GymApp/GYM.GrpcService/Program.cs
@@ -1,12 +1,17 @@
+using GYM.GrpcService.DI;
 using GYM.GrpcService.Services;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+
+builder.Services.AddDependenciesGrpcApi(configuration);
 builder.Services.AddGrpc().AddJsonTranscoding();
 
 builder.Services.AddGrpcSwagger().AddSwaggerGen(c => c.SwaggerDoc("v1",
@@ -22,6 +27,8 @@ app.UseSwaggerUI(c =>
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<CouchesApiService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+ 
